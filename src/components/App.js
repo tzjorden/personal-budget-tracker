8ce167d37c9ab57b1e5be641c3e86c688d01ee64
Mike Starr <imhs@protonmail.com>
2022-03-09 09:52:23 -0800

    Trying to merge with master

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React from "react";
-
 import Tracker from "./Tracker";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Summary from "./Summary";
-
 import Navbar from "./Navbar";
 import ExpenseForm from "./ExpenseForm";
 import IncomeForm from "./IncomeForm";
@@ -12,23 +9,21 @@ import IncomeForm from "./IncomeForm";
 function App() {
   return (
     <div className="App">
-      
       <h1>Personal Budget Tracker</h1>
 
       <Tracker />
 
       <Router>
         <Navbar />
-          <Routes>
-            <Route path='/income'  element={<IncomeForm/>} />
-            <Route path='/expense' element={<ExpenseForm/>} />
-            <Route path='/summary' element={<Summary/>} />
-          </Routes>
+        <Routes>
+          {/* <Route path="/" element={<Tracker.js />} /> */}
+          <Route path="/income" element={<IncomeForm />} />
+          <Route path="/expense" element={<ExpenseForm />} />
+          <Route path="/summary" element={<Summary />} />
+        </Routes>
       </Router>
-
     </div>
   );
 }
 
 export default App;
-
