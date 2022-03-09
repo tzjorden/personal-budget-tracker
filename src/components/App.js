80a0f2bb5fd602964e6f0eacfc1284b92d47ea7d
Tenzin Jorden <tenden96@gmail.com>
2022-03-08 23:36:18 -0800

    added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Summary from "./Summary";
 import Navbar from "./Navbar";
+import ExpenseForm from "./ExpenseForm";
+import IncomeForm from "./IncomeForm";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
       <h1>Personal Budget Tracker</h1>
-      <Summary />
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path='/income'  element={<IncomeForm/>} />
+            <Route path='/expense' element={<ExpenseForm/>} />
+            <Route path='/summary' element={<Summary/>} />
+          </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
